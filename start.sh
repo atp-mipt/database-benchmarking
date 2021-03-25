e9086f9b77c78e6ea7de4673b61e2a5d98c558a9
Mikhail Anukhin <anuhinm@mail.ru>
2021-03-25 21:10:30 +0300

    Update

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 
 export SA_KEY_FILE=~/.ydb/sa_name.json
 cd ydb-python-sdk/examples/basic_example_custom
-python3 __main__.py -e grpcs://ydb.serverless.yandexcloud.net:2135 -d /ru-central1/b1g78gnr21qbtqjat225/etn033rv4tlgsm6elg26
+
+python3 __main__.py -e grpcs://ydb.serverless.yandexcloud.net:2135 -d /ru-central1/b1g78gnr21qbtqjat225/etn033rv4tlgsm6elg26 | tee 1.log & python3 __main__.py -e grpcs://ydb.serverless.yandexcloud.net:2135 -d /ru-central1/b1g78gnr21qbtqjat225/etn033rv4tlgsm6elg26 | tee 2.log
--- transations-transformer.py
+++ transations-transformer.py
@@ -1,7 +1,7 @@
-from io import StringIO
 import clj
 
 with open("txns", "r") as txns_file:
-    objs = clj.loads(txns_file.read())
-    for obj in objs:
-        print(obj)
+    transactions = clj.loads(txns_file.read())
+    for transaction in transactions:
+        for command in transaction['value']:
+            pass
