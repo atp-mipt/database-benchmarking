390992cfb5dfc681ebd61f77088c6e2ed2ae2571
Mikhail Anukhin <anuhinm@mail.ru>
2021-06-30 11:28:22 +0300

    Credentials update

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 export SA_KEY_FILE=~/.ydb/sa_name.json
-export AWS_ACCESS_KEY_ID="dx0_Y_XWiFkIv59RG3J7"
-export AWS_SECRET_ACCESS_KEY="KVZ0FwNrzgU4jQqsD3TOdHg0UVhvU3L5jDV1HHHi"
+export AWS_ACCESS_KEY_ID=""
+export AWS_SECRET_ACCESS_KEY=""
 cd ydb-python-sdk/examples/basic_example_custom
 
 python3 __main__.py -e grpcs://ydb.serverless.yandexcloud.net:2135 -d /ru-central1/b1g78gnr21qbtqjat225/etn033rv4tlgsm6elg26 -pt results/txns-short -n 2 -id 1 -m 0
